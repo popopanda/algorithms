8cab4ffc170d37bc64a2437557ab7a12ed710366
Jeffrey Leung <leung.jeff1120@gmail.com>
2020-05-20 00:42:10 -0700

    use prime nums

--- binary_search/main.go
+++ binary_search/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	primeNums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	primeNums := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
 
-	binarySearch(primeNums, 9)
+	binarySearch(primeNums, 97)
 }
 
 func binarySearch(sliceVar []int, masterNum int) {
