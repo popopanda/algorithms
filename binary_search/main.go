c14fca301abb36c19b198e6e69390cb571874df2
Jeffrey Leung <leung.jeff1120@gmail.com>
2021-01-18 15:19:22 -0800

    test

--- binary_search/main.go
+++ binary_search/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	primeNums := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
 
-	binarySearch(primeNums, 23)
+	binarySearch(primeNums, 3)
 }
 
 func binarySearch(sliceVar []int, masterNum int) {
